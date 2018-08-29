692294263f50318d2afa3305d1e8f1a595201b38
Atakpu Godson Ikhide <ikhide.atakpu@gmail.com>
2018-08-29 18:49:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is my personal profile website hosted on iamikhide.herokuapp.com
+
+Its going to have links to all my personnal projects, pictures and videos of my exciting adventures and later, my tutorials.
+
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+//PROJECTS
+router.get('/music', (req, res, next) => {
+  res.redirect('http://musicianshangout.herokuapp.com');
+});
+
+router.get('/smiley', (req, res, next) => {
+  res.redirect('http://smileyfacer.herokuapp.com');
+});
+
+//SOCIAL MEDIA
+
+router.get('/facebook', (req, res, next) => {
+  res.redirect('https://www.facebook.com/atakpu.ikhide');
+});
+
+router.get('/twitter', (req, res, next) => {
+  res.redirect('https://twitter.com/AtakpuGodson');
+});
+
+router.get('/github', (req, res, next) => {
+  res.redirect('https://github.com/ikhide');
+});
+
+router.get('/google', (req, res, next) => {
+  res.redirect('https://plus.google.com/u/0/107701673017274036288');
+});
+
+router.get('/medium', (req, res, next) => {
+  res.redirect('https://medium.com/@atakpuikhide');
+});
+
+router.get('/stackoverflow', (req, res, next) => {
+  res.redirect('https://stackoverflow.com/users/10202375/atakpu-ikhide');
+});
+module.exports = router;
+
